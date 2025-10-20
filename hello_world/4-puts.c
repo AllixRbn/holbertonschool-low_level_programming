1aa499bedcc167868227360096756e8d13e73cb2
AllixRbn <thewitchonxanax@gmail.com>
2025-10-20 14:26:07 +0000

    fix:  added comment to correct syntax

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - prints a specific sentence to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
