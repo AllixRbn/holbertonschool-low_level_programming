d0a2f2cff4a72283887da796326e2b08f608fa85
AllixRbn <thewitchonxanax@gmail.com>
2025-10-23 17:40:03 +0000

    fix: corrected the if and else if statements

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
 
 	ld = n % 10;
 
-	if (last_digit > 5)
+	if (ld > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-	else if (last_digit == 0)
+	else if (ld == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
