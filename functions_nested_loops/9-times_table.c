ebf239b4a53ffcdcc100192c28f295e87313c557
Allix <thewitchonxanax@gmail.com>
2025-10-26 18:12:18 +0000

    fix: correct output formatting for products in times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,6 +18,13 @@ void times_table(void)
 			{
 				_putchar(product + '0');
 			}
+			else if (product >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
 			else
 			{
 				_putchar(',');
