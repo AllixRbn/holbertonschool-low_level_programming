50205038817cd6be7f5f3160d42bde6f5d156188
AllixRbn <thewitchonxanax@gmail.com>
2025-11-12 15:14:03 +0100

    feat: add create_array function implementation to allocate and initialize an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: character to initialize the array with
+ *
+ * Return: pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+}
