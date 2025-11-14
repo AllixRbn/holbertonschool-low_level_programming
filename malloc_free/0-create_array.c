9650805d2f08c377dfd46ede1702b42d5762d55d
AllixRbn <thewitchonxanax@gmail.com>
2025-11-14 14:42:56 +0100

    fix: correct return values in create_array function to use NULL

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,11 +14,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return (NULL);
+		return (0);
 
 	array = malloc(sizeof(char) * size);
-	if (array == NULL)
-		return (NULL);
+	if (array == 0)
+		return (0);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
